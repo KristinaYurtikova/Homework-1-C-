ce5228b412b3595ba0b916ed75858d193dacbda0
Kristina <kristisha_m@mail.ru>
2022-09-03 00:09:54 +0300

    add task1 new console project

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int num1 = 5;
+int num2 = 7;
+if(num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} меньше числа {num2}");
+}
+    
+
+int num3 = 2;
+int num4 = 10;
+if(num3 > num4)
+{
+    Console.WriteLine($"Число {num3} больше числа {num4}");
+}
+else
+{
+    Console.WriteLine($"Число {num3} меньше числа {num4}");
+}
+
+int num5 = -9;
+int num6 = -3;
+if(num5 > num6)
+{
+    Console.WriteLine($"Число {num5} больше числа {num6}");
+}
+else
+{
+    Console.WriteLine($"Число {num5} меньше числа {num6}");
+}
+
