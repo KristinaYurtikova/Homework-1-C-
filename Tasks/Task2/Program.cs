a6fe0d55545b08498b4a6008ce0731946cd80c84
Kristina <kristisha_m@mail.ru>
2022-09-03 17:34:18 +0300

    update task2

--- Tasks/Task2/Program.cs
+++ Tasks/Task2/Program.cs
@@ -3,9 +3,16 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-int num1 = 2;
-int num2 = 3;
-int num3 = 7;
+Console.WriteLine("Введите первое целое число");
+string input1 = Console.ReadLine(); // пользовательский ввод
+Console.WriteLine("Введите второе целое число");
+string input2 = Console.ReadLine(); // пользовательский ввод
+Console.WriteLine("Введите третье целое число");
+string input3 = Console.ReadLine(); // пользовательский ввод
+
+int num1 = int.Parse (input1);
+int num2 = int.Parse (input2);
+int num3 = int.Parse (input3);
 
 int max = num1;
 
@@ -16,28 +23,3 @@ if (num3 > max) max = num3;
 Console.Write("max= ");
 Console.WriteLine(max);
 
-int num4 = 44;
-int num5 = 5;
-int num6 = 78;
-
-int max2 = num4;
-
-if (num4 > max) max = num4;
-if (num5 > max) max = num5;
-if (num6 > max) max = num6;
-
-Console.Write("max= ");
-Console.WriteLine(max);
-
-int num7 = 22;
-int num8 = 3;
-int num9 = 9;
-
-int max3 = num7;
-
-if (num7 > max) max = num7;
-if (num8 > max) max = num8;
-if (num9 > max) max = num9;
-
-Console.Write("max= ");
-Console.WriteLine(max3);
