a6bd1aa6224d1858aa1459b96df1cf40730c8534
Kristina <kristisha_m@mail.ru>
2022-09-03 17:22:14 +0300

    add task4 new console project

--- Tasks/Task4/Program.cs
+++ Tasks/Task4/Program.cs
@@ -2,3 +2,11 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+Console.WriteLine("Введите целое число n от 1 до бесконечности");
+string input = Console.ReadLine(); // пользовательский ввод
+int n = int.Parse (input);
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+         Console.WriteLine(i);
+
+
